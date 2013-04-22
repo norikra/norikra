ccff086fd0086f860ae9e16d6ddcfb712412126c
TAGOMORI Satoshi <tagomoris@gmail.com>
2013-04-22 19:43:17 +0900

    fix to have time from epoch of each events

--- lib/norikra/output_pool.rb
+++ lib/norikra/output_pool.rb
@@ -1,18 +1,26 @@
 module Norikra
   class OutputPool
+    attr_accessor :pool
+
     def initialize
       @pool = {}
       @mutex = Mutex.new
     end
 
     def push(tablename, events)
-      events = events.map(&:getUnderlying)
+      t = Time.now.to_i
+      if events.first.respond_to?(:getUnderlying) # easy to test...
+        events = events.map{|e| [t, e.getUnderlying]}
+      else
+        events = events.map{|e| [t, e]}
+      end
       @mutex.synchronize do
         @pool[tablename] ||= []
         @pool[tablename] += events
       end
     end
 
+    # returns [time(int from epoch), event], event: hash
     def pop(tablename)
       @mutex.synchronize do
         @pool.delete(tablename) || []
