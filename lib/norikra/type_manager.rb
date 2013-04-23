574462084e525852fae64251b814f9f8f2add41a
TAGOMORI Satoshi <tagomoris@gmail.com>
2013-04-23 12:01:41 +0900

    writing

--- lib/norikra/type_manager.rb
+++ lib/norikra/type_manager.rb
@@ -1,4 +1,13 @@
+require 'norikra/typedef'
+
 module Norikra
   class TypeManager
+    def initialize(opts)
+      @strict_check = opts[:strict] || false
+      @dict = {} # tablename => [typedefs]
+    end
+
+    def refer(tablename, data_batch)
+    end
   end
 end
