5b651b0a06beb338cfdbb5568808e115a1976801
TAGOMORI Satoshi <tagomoris@gmail.com>
2013-04-23 18:07:18 +0900

    add type definition generation and matching

--- lib/norikra/typedef.rb
+++ lib/norikra/typedef.rb
@@ -9,6 +9,54 @@ module Norikra
       @name = param[:name] || Digest::MD5.hexdigest(@definition.inspect)
     end
 
+    # string  A single character to an unlimited number of characters.
+    # boolean A boolean value.
+    # integer An integer value (4 byte).
+    # long    A long value (8 byte). Use the "L" or "l" (lowercase L) suffix. # select 1L as field1, 1l as field2
+    # double  A double-precision 64-bit IEEE 754 floating point.              # select 1.67 as field1, 167e-2 as field2, 1.67d as field3
+    # float   A single-precision 32-bit IEEE 754 floating point. Use the "f" suffix. # select 1.2f as field1, 1.2F as field2
+    # byte    A 8-bit signed two's complement integer.                        # select 0x10 as field1
+    def match?(target)
+      target.keys.each do |k|
+        return false unless @definition.has_key?(k)
+
+        type = @definition[k]
+        value = target[k]
+
+        ret = case type
+              when 'string'
+                value.is_a?(String)
+              when 'boolean'
+                value.is_a?(TrueClass) || value.is_a?(FalseClass) || (value.is_a?(String) && value =~ /^(?:true|false)$/i)
+              when 'long', 'integer'
+                value.is_a?(Integer) || (value.is_a?(String) && value =~ /^-?\d+[lL]?$/)
+              when 'double', 'float'
+                value.is_a?(Float) || value.is_a?(Integer) || (value.is_a?(String) && value =~ /^-?\d+(\.\d+)?(?:[eE]-?\d+|[dDfF])?$/)
+              end
+        return false unless ret
+      end
+      true
+    end
+
+    def self.guess(data)
+      definition = {}
+      data.keys.each do |key|
+        val = data[key]
+        sval = val.to_s
+        case
+        when val.is_a?(TrueClass) || val.is_a?(FalseClass) || sval =~ /^(?:true|false)$/i
+          definition[key.to_s] = 'boolean'
+        when val.is_a?(Integer) || sval =~ /^-?\d+[lL]?$/
+          definition[key.to_s] = 'long'
+        when val.is_a?(Float) || sval =~ /^-?\d+\.\d+(?:[eE]-?\d+|[dDfF])?$/
+          definition[key.to_s] = 'double'
+        else
+          definition[key.to_s] = 'string'
+        end
+      end
+      self.new(:definition => definition)
+    end
+
     def self.mangle_symbols(hash)
       ret = hash.dup
       ret.keys.select{|k| k.is_a?(Symbol)}.each do |s|
--- spec/typedef_spec.rb
+++ spec/typedef_spec.rb
@@ -11,11 +11,51 @@ describe Norikra::Typedef do
         Norikra::Typedef.mangle_symbols({'a' => 1, :b => 2}).should == {'a' => 1, 'b' => 2}
       end
     end
+
+    describe '.guess' do
+      it 'can guess Boolean values and boolean-like strings as boolean correctly' do
+        t = Norikra::Typedef.guess({'key1' => true, 'key2' => false, 'key3' => 'True', 'key4' => 'TRUE', 'key5' => 'false'})
+        r = t.definition
+        r['key1'].should eql('boolean')
+        r['key2'].should eql('boolean')
+        r['key3'].should eql('boolean')
+        r['key4'].should eql('boolean')
+        r['key5'].should eql('boolean')
+      end
+
+      it 'can guess Float values and floating point number like strings as double correctly' do
+        t = Norikra::Typedef.guess({'key1' => 0.1, 'key2' => 1.571, 'key3' => '1.571', 'key4' => '1.571e10', 'key5' => '-1.57e-5'})
+        r = t.definition
+        r['key1'].should eql('double')
+        r['key2'].should eql('double')
+        r['key3'].should eql('double')
+        r['key4'].should eql('double')
+        r['key5'].should eql('double')
+      end
+      it 'can guess Integer values and int number like strings as long correctly' do
+        t = Norikra::Typedef.guess({'key1' => 1, 'key2' => (2**32 + 1), 'key3' => '1024', 'key4' => '4294967297', 'key5' => '-3017'})
+        r = t.definition
+        r['key1'].should eql('long')
+        r['key2'].should eql('long')
+        r['key3'].should eql('long')
+        r['key4'].should eql('long')
+        r['key5'].should eql('long')
+      end
+      it 'guess all non-boolean-float-integer values as string' do
+        t = Norikra::Typedef.guess({'key1' => 'foo bar', 'key2' => '', 'key3' => nil, 'key4' => 'NULL', 'key5' => [1,2,3]})
+        r = t.definition
+        r['key1'].should eql('string')
+        r['key2'].should eql('string')
+        r['key3'].should eql('string')
+        r['key4'].should eql('string')
+        r['key5'].should eql('string')
+      end
+    end
   end
 
   context 'when init' do
     describe '#initialize' do
-      definition = {:service => 'string', :path => 'string', :duration => 'int'}
+      definition = {:service => 'string', :path => 'string', :duration => 'long'}
 
       context 'with name parameter' do
         subject { Norikra::Typedef.new(
@@ -29,6 +69,51 @@ describe Norikra::Typedef do
         subject { Norikra::Typedef.new(:definition => definition) }
         its(:definition) { should == Norikra::Typedef.mangle_symbols(definition) }
         its(:name) { should == Digest::MD5.hexdigest(Norikra::Typedef.mangle_symbols(definition).inspect) }
+      end
+    end
+  end
+
+  context 'when instanciated' do
+    definition = {:service => 'string', :path => 'string', :duration => 'long', :rate => 'double'}
+    typedef = Norikra::Typedef.new(:definition => definition)
+
+    describe '#match' do
+      context 'with objects looks matches' do
+        target1 = {'service' => 'foobar', 'path' => '/path/to/data', 'duration' => 103200, 'rate' => 0.33}
+        target2 = {'service' => 'foobar', 'path' => '/path/to/data', 'duration' => '103200', 'rate' => '0.33'}
+        target3 = {'service' => 'foobar', 'path' => '/path/to/data', 'duration' => '103200', 'rate' => 33}
+        it 'returns true' do
+          typedef.match?(target1).should be_true
+          typedef.match?(target2).should be_true
+          typedef.match?(target3).should be_true
+        end
+      end
+
+      context 'with objects looks doesn\'t matches' do
+        target1 = {'service' => 1.03, 'path' => '/path/to/data', 'duration' => 103200, 'rate' => '0.33'}
+        target2 = {'service' => 'foobar', 'path' => '/path/to/data', 'duration' => 0.11, 'rate' => '0.33'}
+        target3 = {'service' => 'foobar', 'path' => '/path/to/data', 'duration' => 103200, 'rate' => '3handreds'}
+        target4 = {'service' => 'foobar', 'path' => '/path/to/data', 'duration' => 103200, 'rate' => 'NULL'}
+        it 'returns false' do
+          typedef.match?(target1).should be_false
+          typedef.match?(target2).should be_false
+          typedef.match?(target3).should be_false
+          typedef.match?(target4).should be_false
+        end
+      end
+    end
+  end
+
+  context 'when instanciated through .guess' do
+    describe '#match' do
+      target1 = {'service' => 'foobar', 'path' => '/path/to/data', 'duration' => 103200, 'rate' => 0.33}
+      target2 = {'service' => 'foobar', 'path' => '/path/to/data', 'duration' => '103200', 'rate' => '0.33'}
+      target3 = {'service' => 'foobar', 'path' => '/path/to/data', 'duration' => '103200', 'rate' => 33}
+
+      it 'returns true with self definition' do
+        Norikra::Typedef.guess(target1).match?(target1).should be_true
+        Norikra::Typedef.guess(target2).match?(target2).should be_true
+        Norikra::Typedef.guess(target3).match?(target3).should be_true
       end
     end
   end
