fed05ca846ca99e19c6213849b7ae6516a578ebd
TAGOMORI Satoshi <tagomoris@gmail.com>
2013-05-13 20:15:04 +0900

    add #to_hash for msgpack serialization

--- lib/norikra/query.rb
+++ lib/norikra/query.rb
@@ -1,13 +1,15 @@
-require 'norikra/typedef'
-
 module Norikra
   class Query
-    attr_accessor :name, :tablename, :typedef, :expression
+    attr_accessor :name, :tablename, :expression
 
     def initialize(param={})
       @name = param[:name]
       @tablename = param[:tablename]
       @expression = param[:expression]
+    end
+
+    def to_hash
+      {'name' => @name, 'tablename' => @tablename, 'expression' => @expression}
     end
   end
 end
