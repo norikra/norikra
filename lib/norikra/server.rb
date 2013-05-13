37378442d4a0e1cb6f53523c0aa8d448017c8959
TAGOMORI Satoshi <tagomoris@gmail.com>
2013-05-13 20:14:32 +0900

    specify default port, using norikura mountains heights

--- lib/norikra/server.rb
+++ lib/norikra/server.rb
@@ -9,11 +9,14 @@ require 'norikra/rpc'
 
 module Norikra
   class Server
-    def initialize
+    RPC_DEFAULT_PORT = 26571
+    # 26571 = 3026 + 3014 + 2968 + 2950 + 2891 + 2896 + 2975 + 2979 + 2872
+
+    def initialize(host=nil, port=RPC_DEFAULT_PORT)
       @typedef_manager = Norikra::TypedefManager.new
       @output_pool = Norikra::OutputPool.new
       @engine = Norikra::Engine.new(@output_pool, @typedef_manager)
-      @rpcserver = Norikra::RPC::HTTP.new(:engine => @engine, :port => xxx)
+      @rpcserver = Norikra::RPC::HTTP.new(:engine => @engine, :port => port)
     end
 
     def run
