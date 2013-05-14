cec9962a02d6238f78ba99b8e784cd2904e01890
TAGOMORI Satoshi <tagomoris@gmail.com>
2013-05-14 22:21:38 +0900

    temporary server startup

--- lib/norikra/server.rb
+++ lib/norikra/server.rb
@@ -9,12 +9,16 @@ require 'norikra/rpc'
 
 module Norikra
   class Server
+    RPC_DEFAULT_HOST = '0.0.0.0'
     RPC_DEFAULT_PORT = 26571
     # 26571 = 3026 + 3014 + 2968 + 2950 + 2891 + 2896 + 2975 + 2979 + 2872
 
-    def initialize(host=nil, port=RPC_DEFAULT_PORT)
+    def initialize(host=RPC_DEFAULT_HOST, port=RPC_DEFAULT_PORT, configuration={})
+      #TODO: initial configuration
       @typedef_manager = Norikra::TypedefManager.new
+
       @output_pool = Norikra::OutputPool.new
+
       @engine = Norikra::Engine.new(@output_pool, @typedef_manager)
       @rpcserver = Norikra::RPC::HTTP.new(:engine => @engine, :port => port)
     end
@@ -22,6 +26,10 @@ module Norikra
     def run
       @engine.start
       @rpcserver.start
+      p "Norikra server started."
+      #TODO: main loop and signal traps
+      #TODO: loggings
+      sleep 50
     end
 
     def shutdown
