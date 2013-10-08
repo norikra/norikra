87d42549704d59a0ca063de92b40e92678987af1
TAGOMORI Satoshi <tagomoris@gmail.com>
2013-10-08 19:30:16 +0900

    add helpers to test with engine instance

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -46,10 +46,46 @@ end
 # These instructions should self-destruct in 10 seconds.  If they don't, feel
 # free to delete them.
 
-
-
-
 require 'rubygems'
 require 'rspec'
 $LOAD_PATH.unshift(File.dirname(__FILE__) + '/../lib')
 $LOAD_PATH.unshift(File.dirname(__FILE__) + '/../esper')
+
+require 'norikra/logger'
+
+$dummylogger = Norikra::DummyLogger.new
+Norikra::Log.init('test', nil, {:logger => $dummylogger})
+
+require 'norikra/engine'
+
+$running = {}
+
+module Norikra::SpecHelper
+  def logger ; $dummylogger ; end
+  def engine_start
+    service = com.espertech.esper.client.EPServiceProviderManager.getDefaultProvider
+    administrator = service.getEPAdministrator
+    config = administrator.getConfiguration
+    runtime = service.getEPRuntime
+    $running = {
+      service: service,
+      administrator: administrator,
+      config: config,
+      runtime: runtime,
+    }
+  end
+  def engine_stop
+    administrator.stopAllStatements
+    $running = {}
+  end
+  def with_engine
+    engine_start
+    val = yield
+    engine_stop
+    val
+  end
+  def service ; $running[:service] ; end
+  def config ; $running[:config] ; end
+  def runtime ; $running[:runtime] ; end
+  def administrator ; $running[:administrator] ; end
+end
