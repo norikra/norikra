cfa2b785cdee31bafca2940fbdfa39c440a99ba0
TAGOMORI Satoshi <tagomoris@gmail.com>
2014-06-02 18:47:25 +0900

    move listener to outside of engine for tests

--- lib/norikra/engine.rb
+++ lib/norikra/engine.rb
@@ -2,6 +2,7 @@ require 'java'
 
 require 'norikra/error'
 require 'norikra/target'
+require 'norikra/listener'
 
 require 'norikra/logger'
 include Norikra::Log
@@ -239,73 +240,6 @@ module Norikra
       load_udf(plugin_klass)
     end
 
-    class Listener
-      include com.espertech.esper.client.UpdateListener
-
-      def initialize(query_name, query_group, output_pool, events_statistics)
-        @query_name = query_name
-        @query_group = query_group
-        @output_pool = output_pool
-        @events_statistics = events_statistics
-      end
-
-      def type_convert(value)
-        if value.respond_to?(:getUnderlying)
-          value = value.getUnderlying
-        end
-
-        trace "converting", :value => value
-
-        if value.respond_to?(:to_hash)
-          Hash[ value.to_hash.map{|k,v| [ Norikra::Field.unescape_name(k), type_convert(v)] } ]
-        elsif value.respond_to?(:to_a)
-          value.to_a.map{|v| type_convert(v) }
-        elsif value.respond_to?(:force_encoding)
-          value.force_encoding('UTF-8')
-        else
-          value
-        end
-      end
-
-      def update(new_events, old_events)
-        t = Time.now.to_i
-        events = new_events.map{|e| [t, type_convert(e)]}
-        trace "updated event", :query => @query_name, :group => @query_group, :event => events
-        @output_pool.push(@query_name, @query_group, events)
-        @events_statistics[:output] += events.size
-      end
-    end
-
-    class LoopbackListener < Listener
-      def initialize(engine, query_name, query_group, events_statistics)
-        @engine = engine
-        @query_name = query_name
-        @query_group = query_group
-        @events_statistics = events_statistics
-        @loopback_target = Norikra::Query.loopback(query_group)
-      end
-
-      def update(new_events, old_events)
-        t = Time.now.to_i
-        event_list = new_events.map{|e| type_convert(e) }
-        trace "loopback event", :query => @query_name, :group => @query_group, :event => event_list
-        @events_statistics[:output] += event_list.size
-        #
-        # We does NOT convert 'container.$0' into container['field'].
-        # Use escaped names like 'container__0'. That is NOT so confused.
-        @engine.send(@loopback_target, event_list)
-      end
-    end
-
-    ##### Unmatched events are simply ignored
-    # class UnmatchedListener
-    #   include com.espertech.esper.client.UnmatchedListener
-    #   def update(event)
-    #     # puts "unmatched:\n- " + event.getProperties.inspect
-    #     # ignore
-    #   end
-    # end
-
     private
 
     def open_target(target)
@@ -484,9 +418,9 @@ module Norikra
       Norikra::Query.rewrite_query(statement_model, event_type_name_map)
 
       listener = if Norikra::Query.loopback(query.group)
-                   LoopbackListener.new(self, query.name, query.group, @statistics[:events])
+                   Norikra::LoopbackListener.new(self, query.name, query.group, @statistics[:events])
                  else
-                   Listener.new(query.name, query.group, @output_pool, @statistics[:events])
+                   Norikra::Listener.new(query.name, query.group, @output_pool, @statistics[:events])
                  end
 
       epl = administrator.create(statement_model)
